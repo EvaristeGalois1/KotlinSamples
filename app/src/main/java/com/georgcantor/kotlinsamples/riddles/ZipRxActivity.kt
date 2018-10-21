287aab0c7995c7bda43691aa4a3fd0181aa51479
GeorgCantor <cupsman1986@gmail.com>
2018-10-21 13:05:01 +0700

    rx: added methods with Observables

--- app/src/main/java/com/georgcantor/kotlinsamples/riddles/ZipRxActivity.kt
+++ app/src/main/java/com/georgcantor/kotlinsamples/riddles/ZipRxActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.georgcantor.kotlinsamples.R
 import com.georgcantor.kotlinsamples.riddles.model.Player
+import com.georgcantor.kotlinsamples.riddles.utils.UtilRx
+import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_zip_rx.*
-
-
 
 
 class ZipRxActivity : AppCompatActivity() {
@@ -20,6 +20,26 @@ class ZipRxActivity : AppCompatActivity() {
 
     fun sortNamesakes() {
 
+    }
+
+    private fun footballplayers(): Observable<List<Player>> {
+        return Observable
+                .create<List<Player>> { it ->
+                    if (!it.isDisposed) {
+                        it.onNext(UtilRx.footballPLayers())
+                        it.onComplete()
+                    }
+                }
+    }
+
+    private fun hockeyPlayers(): Observable<List<Player>> {
+        return Observable
+                .create<List<Player>> { it ->
+                    if (!it.isDisposed) {
+                        it.onNext(UtilRx.hockeyPlayers())
+                        it.onComplete()
+                    }
+                }
     }
 
     private fun getObserver(): Observer<List<Player>> {
--- app/src/main/java/com/georgcantor/kotlinsamples/riddles/utils/UtilRx.kt
+++ app/src/main/java/com/georgcantor/kotlinsamples/riddles/utils/UtilRx.kt
@@ -4,38 +4,39 @@ import com.georgcantor.kotlinsamples.riddles.model.Player
 
 class UtilRx {
 
-    fun footballPLayers(): ArrayList<Player> {
+    companion object {
 
-        val list = ArrayList<Player>()
+        fun footballPLayers(): ArrayList<Player> {
+            val list = ArrayList<Player>()
 
-        val playerOne = Player()
-        playerOne.firstName = "Tom"
-        playerOne.lastName = "Dwain"
-        list.add(playerOne)
+            val playerOne = Player()
+            playerOne.firstName = "Tom"
+            playerOne.lastName = "Dwain"
+            list.add(playerOne)
 
-        val playerTwo = Player()
-        playerTwo.firstName = "Danny"
-        playerTwo.lastName = "Lawrence"
-        list.add(playerTwo)
+            val playerTwo = Player()
+            playerTwo.firstName = "Danny"
+            playerTwo.lastName = "Lawrence"
+            list.add(playerTwo)
 
-        return list
-    }
+            return list
+        }
 
-    fun hockeyPlayers(): ArrayList<Player> {
+        fun hockeyPlayers(): ArrayList<Player> {
+            val list = ArrayList<Player>()
 
-        val list = ArrayList<Player>()
+            val playerOne = Player()
+            playerOne.firstName = "Anthony"
+            playerOne.lastName = "Dwain"
+            list.add(playerOne)
 
-        val playerOne = Player()
-        playerOne.firstName = "Anthony"
-        playerOne.lastName = "Dwain"
-        list.add(playerOne)
+            val playerTwo = Player()
+            playerTwo.firstName = "Sean"
+            playerTwo.lastName = "Smith"
+            list.add(playerTwo)
 
-        val playerTwo = Player()
-        playerTwo.firstName = "Sean"
-        playerTwo.lastName = "Smith"
-        list.add(playerTwo)
-
-        return list
+            return list
+        }
     }
 
     fun filterNamesakes(footballPlayers: ArrayList<Player>, hockeyPlayers: ArrayList<Player>) {
